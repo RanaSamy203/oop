597fba3aff18194dff06ffd567317c03ffc775bf
mazennasser_ <mazennasser673@gmail.com>
2025-10-03 17:57:27 +0300

    edit in filter 4 and 10

--- mazen.cpp
+++ mazen.cpp
@@ -33,68 +33,133 @@ void LightDark(Image &img, float factor) {
     }
 }
 
+Image merge_images(Image &image1) {
+    string filename_2;
+    cout << "Pls enter second image path: ";
+    cin >> filename_2;
+    Image image2(filename_2);
+    if (!image2.imageData) {
+        cerr << "Failed to load second image!" << endl;
+        return image1;
+    }
+    Image new_image(image2.width, image2.height);
+    new_image.channels = image1.channels;
+    for (int i = 0; i < new_image.width; i++) {
+        for (int j = 0; j < new_image.height; j++) {
+            for (int k = 0; k < new_image.channels; k++) {
+                new_image(i, j, k) =
+                    image1(int(i * double(image1.width) / new_image.width),
+                           int(j * double(image1.height) / new_image.height), k);
+            }
+        }
+    }
+    for (int i = 0; i < new_image.width; i++) {
+        for (int j = 0; j < new_image.height; j++) {
+            for (int k = 0; k < new_image.channels; k++) {
+                new_image(i, j, k) =
+                    (image2(i, j, k) + new_image(i, j, k)) / 2;
+            }
+        }
+    }
+    return new_image;
+}
+
+Image detect_image_edges(Image &image) {
+    Image img = image;
+    gray_scale(img);
+    BandW(img);
+    int width = img.width;
+    int height = img.height;
+    Image edges(width, height);
+    for (int y = 1; y < height - 1; ++y) {
+        for (int x = 1; x < width - 1; ++x) {
+            int Gx = img(x + 1, y, 0) - img(x - 1, y, 0);
+            int Gy = img(x, y + 1, 0) - img(x, y - 1, 0);
+            if (abs(Gx) + abs(Gy) > 128) {
+                edges(x, y, 0) = edges(x, y, 1) = edges(x, y, 2) = 0;
+            } else {
+                edges(x, y, 0) = edges(x, y, 1) = edges(x, y, 2) = 255;
+            }
+        }
+    }
+    return edges;
+}
+
 int main() {
     string filename;
     cout << "Enter the filename of the image: ";
     cin >> filename;
     Image originalImg(filename);
-
     if (!originalImg.imageData) {
         cerr << "Failed to load image, Please use .bmp, .jpg, .jpeg, or .png: " << filename << endl;
         return -1;
     }
-
     while (true) {
         cout << "0 - Exit\n";
         cout << "1 - Grayscale Filter\n";
         cout << "2 - Lighten/Darken Filter\n";
-
+        cout << "3 - Merge Images\n";
+        cout << "4 - Detect Image Edges\n";
         int choice;
         cin >> choice;
-
         switch (choice) {
             case 0:
-                cout << "Exiting the program.\n";
                 return 0;
-
             case 1: {
                 Image img = originalImg;
                 gray_scale(img);
-                cout << "Grayscale filter applied.\n";
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 string saveFile;
                 cout << "Enter filename to save the image or press Enter to skip: ";
                 getline(cin, saveFile);
                 if (!saveFile.empty()) {
                     img.saveImage(saveFile);
-                    cout << "Image saved as " << saveFile << "\n";
                 }
                 break;
             }
-
             case 2: {
                 Image img = originalImg;
                 float factor;
                 cout << "Enter factor (e.g. >1 to lighten, <1 to darken): ";
                 cin >> factor;
                 LightDark(img, factor);
-                cout << "Lighten/Darken filter applied.\n";
                 cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 string saveFile;
                 cout << "Enter filename to save the image or press Enter to skip: ";
                 getline(cin, saveFile);
                 if (!saveFile.empty()) {
                     img.saveImage(saveFile);
-                    cout << "Image saved as " << saveFile << "\n";
                 }
                 break;
             }
-
+            case 3: {
+                Image img = originalImg;
+                img = merge_images(img);
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                string saveFile;
+                cout << "Enter filename to save the image or press Enter to skip: ";
+                getline(cin, saveFile);
+                if (!saveFile.empty()) {
+                    img.saveImage(saveFile);
+                }
+                break;
+            }
+            case 4: {
+                Image img = detect_image_edges(originalImg);
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                string saveFile;
+                cout << "Enter filename to save the image or press Enter to skip: ";
+                getline(cin, saveFile);
+                if (!saveFile.empty()) {
+                    img.saveImage(saveFile);
+                }
+                break;
+            }
             default:
-                cout << "Invalid choice. Please try again.\n";
                 break;
         }
     }
-
     return 0;
 }
+
+
